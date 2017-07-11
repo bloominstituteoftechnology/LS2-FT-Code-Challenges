942b55ded0b8ac7c013b8828f150c336dd8bad2f
Dan Winslow <dwinslow123@me.com>
2017-07-11 11:51:58 -0500

    longestString complete

--- longestString/longestString.js
+++ longestString/longestString.js
@@ -2,3 +2,17 @@
  * Write a function that accepts an array of strings.
  * Return the longest string in the array.
  */
+
+ const findLongestArray = array => {
+  let length = 0; 
+  let longestArr;
+  for ( let i = 0; i < array.length; i++ ) {
+    if ( array[i].length > length ) {
+      length = array[i].length;
+      longestArr = array[i];
+    }
+  }
+  return longestArr; 
+ }
+
+ findLongestArray(['John', 'Paul', 'George', 'Ringo'])
