68ca4c8f6468cbd2c876b61b7388e86e32ec00b8
Brandon Moore <mbrando2@gmail.com>
2017-07-11 11:03:53 -0600

    Created function, 'giveDatLongWord',failure

--- longestString/longestString.js
+++ longestString/longestString.js
@@ -2,3 +2,17 @@
  * Write a function that accepts an array of strings.
  * Return the longest string in the array.
  */
+const giveDatLongWord = (array) => {
+	var array = ["Pseudopseudohypoparathyroidism", "Floccinaucinihilipilification", "Antidisestablishmentarianism",
+	"supercalifragilisticexpialidocious","Incomprehensibilities"];
+	var longestString = array[0];
+	for(var i = 0; i < array.length; i++){
+		newLongest = array[i];
+		if (newLongest.length > longestString.length){
+			longestString = newLongest;
+		}
+	}
+	longestString(array);
+	return longestString;
+}
+
