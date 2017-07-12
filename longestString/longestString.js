f4c723794a8eb4336c991505c2a28d2c0eb1ac0d
Brandon Moore <mbrando2@gmail.com>
2017-07-12 10:18:16 -0600

    my commit message

--- longestString/longestString.js
+++ longestString/longestString.js
@@ -1,18 +1,6 @@
-/*
- * Write a function that accepts an array of strings.
- * Return the longest string in the array.
- */
-const giveDatLongWord = (array) => {
-	var array = ["Pseudopseudohypoparathyroidism", "Floccinaucinihilipilification", "Antidisestablishmentarianism",
-	"supercalifragilisticexpialidocious","Incomprehensibilities"];
-	var longestString = array[0];
-	for(var i = 0; i < array.length; i++){
-		newLongest = array[i];
-		if (newLongest.length > longestString.length){
-			longestString = newLongest;
-		}
-	}
-	longestString(array);
-	return longestString;
-}
-
+const longestString = (strArr) => {
+   	let longest = '';
+   	for (let i = 0; i < strArr.length; i++) {
+   		if (strArr[i].length > longest.length) longest = strArr[i];
+   	}
+   	return longest;
