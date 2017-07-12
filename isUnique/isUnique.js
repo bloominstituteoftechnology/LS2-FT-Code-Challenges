4084c2363af190029be437b9c56949159e9dd254
Gregory Knudsen <dev.artist15@gmail.com>
2017-07-12 12:25:31 -0400

    version works...want to refactor

--- isUnique/isUnique.js
+++ isUnique/isUnique.js
@@ -1,12 +1,16 @@
-/* 
+/*
  * Implement an algorithm to determine if a string has all unique characters.
  * Extra Credit - Answer this question - What if you cannot use additional data structures?
  */
 
 const isUnique = (str) => {
-
+  const strArr = str.split('').sort();
+  for (let i = 0; i < strArr.length; i++) {
+    if (strArr[i] === strArr[i + 1]) {
+      return false;
+    } return true;
+  }
 };
 
 console.log(isUnique('abcdhijklmnopqrstuv')); // true
 console.log(isUnique('abcdefga')); // false
-
