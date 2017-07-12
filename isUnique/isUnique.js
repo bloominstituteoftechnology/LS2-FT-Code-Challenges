cbe94ca803a523ead37d42d017afbe91506dd4e5
Gregory Knudsen <dev.artist15@gmail.com>
2017-07-12 12:51:03 -0400

    use indexOf to prevent having to use array or object

--- isUnique/isUnique.js
+++ isUnique/isUnique.js
@@ -6,9 +6,27 @@
 const isUnique = (str) => {
   const strArr = str.split('').sort();
   for (let i = 0; i < strArr.length; i++) {
-  return strArr[i] === strArr[i + 1] ? false : true
+    return strArr[i] === strArr[i + 1] ? false : true
   }
 };
 
 console.log(isUnique('abcdhijklmnopqrstuv')); // true
 console.log(isUnique('abcdefga')); // false
+
+console.log('-----------')
+console.log('***String Only***')
+
+const isUniqueStringOnly = (str) => {
+
+  for (let i = 0; i < str.length; i++) {
+    const ch = str[i];
+    if (str.indexOf(ch, i + 1) > -1) {
+      return false;
+    }
+    return true;
+  }
+};
+
+console.log(isUniqueStringOnly('abcdhijklmnopqrstuv')); // true
+console.log(isUniqueStringOnly('abcdefga')); // false
+console.log(isUniqueStringOnly('abcdebbba')); // false
