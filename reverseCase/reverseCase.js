09b005bc7852206c44017a5bdadcd9b9aca5f62d
Gregory Knudsen <dev.artist15@gmail.com>
2017-07-13 12:03:44 -0400

    tests added

--- reverseCase/reverseCase.js
+++ reverseCase/reverseCase.js
@@ -3,3 +3,8 @@
  * Example: 'Hello World' -> 'hELLO wORLD'
  * Assume that each string will contain only spaces and letters.
  */
+
+
+
+console.log(reverseCase('Hello World')); // 'hELLO wORLD'
+console.log(reverseCase('How Are You Today')); // 'hOW aRE yOU tODAY'
