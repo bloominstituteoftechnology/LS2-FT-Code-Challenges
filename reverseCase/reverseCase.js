707ede379a6b1ee39c4ffcacb2533057939c6fe0
Nana Owusu Nyamekye <nanaowusu1234@gmail.com>
2017-07-13 18:45:10 +0000

    Solution To 'reverseCase.js'

--- reverseCase/reverseCase.js
+++ reverseCase/reverseCase.js
@@ -3,3 +3,18 @@
  * Example: 'Hello World' -> 'hELLO wORLD'
  * Assume that each string will contain only spaces and letters.
  */
+
+ const caseChanger = (str) => {
+     let transformer = "";
+     for (let i = 0; i < str.length; i++) {
+         if (str[i] === str[i].toUpperCase())
+            transformer = transformer + str[i].toLowerCase();
+         else if (str[i] === str[i].toLowerCase())
+            transformer = transformer + str[i].toUpperCase();
+     }
+
+     return transformer;
+ }
+
+ console.log(caseChanger('naNa OWUsU NyaMekYe'));
+ console.log(caseChanger('Hello World'));
