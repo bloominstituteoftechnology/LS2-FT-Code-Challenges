bf71f9968d25e54f4873014f85129224a31dd087
Colombianito66 <colombianito66@gmail.com>
2017-07-15 00:37:35 -0400

    largest prime palindrome not complete

---
+++ largestPrimePalindrome/src/largestPrimePalindrome.js
@@ -0,0 +1,37 @@
+
+/*
+ * Create a function that returns the largest prime palindrome less than 1000.
+ * Hint: it's 929
+ * You will first want to determine if the number is a palindrome and then determine if it is prime.
+ * A palindrome is a number that is the same forwards and backwards: 121, 323, 123454321, etc.
+ */ 
+ // I have an array of 1000.
+ // create array that will hold my palindromes;
+ // create array that will hold my prime numbers;
+ // iterate through the array of numbers.
+ // if element === element.reverse(), place it in palindromes array.
+ // Now, iterate through palindromes array
+ // (let i = 2; i < array[i]; i++)
+ // a number is prime if it is only divisible by itself and 1.
+ // if (element % i !== 0), number is prime.
+ // add element to prime numbers array.
+ // sort the prime numbers array.
+ 
+const largestPrimePalindrome = () => {
+ 
+ const palindromesArray = [];
+ const primeArray = [];
+
+ for (let i = 0; i < 1000; i++) {
+ 	let str = i.toString();
+
+ 	if (str === str.reverse()) palindromesArray.push(i);
+ }
+ for (let i = 0; i < palindromesArray.length; i++) {
+ 	for (let i = 2; i < palindromesArray[i]; i++) {
+ 	if (palindromesArray[i] % i !== 0) primeArray.push(palindromesArray[i]);
+ 	primeArray.sort();
+   }
+  console.log(primeArray);
+ }
+};
