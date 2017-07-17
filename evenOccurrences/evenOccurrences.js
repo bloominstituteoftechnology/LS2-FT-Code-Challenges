8cfea0ad79ec83f69cb116ea61227febcdf85dd5
Allan Felipe <allan.murara@gmail.com>
2017-07-17 17:25:11 -0300

    Finished Daily code challenge

--- evenOccurrences/evenOccurrences.js
+++ evenOccurrences/evenOccurrences.js
@@ -10,6 +10,30 @@
  *    * console.log(onlyEven); //  4
  *    */
 
+//should iterate over array and check if it already exists with set
 const evenOccurence = (arr) => {
-  // Your code here.
+	const set = new Set();
+  // go around set and remove current position of the array 
+  // 
+  const tail = (arr, index) => {
+    let newarray = [];
+    const getelement = (arr, index, pos2 = arr.length) => {
+      if (index === pos2) { return newarray }
+      newarray.push(arr[index])
+      index++;
+      return getelement(arr,index);
+    } 
+    return getelement(arr, index)
+  }
+	const iteration = (arr, index = 0) => {
+   if (index === arr.length ) {  return null; }
+   if (new Set(tail(arr,index+1)).has(arr[index])) return arr[index];
+   index++;
+   return iteration(arr, index)
+	}
+  return iteration(arr)
 };
+
+const onlyEven = evenOccurence([1, 7,1, 2,7,3, 4, 5, 6, 8, 9]);
+
+
