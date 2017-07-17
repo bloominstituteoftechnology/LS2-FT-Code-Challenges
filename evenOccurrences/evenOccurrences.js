e88dbfeb42533e542e4b6eac453af241cd9d897c
Gregory Knudsen <dev.artist15@gmail.com>
2017-07-17 13:02:45 -0400

    set up plan for solution in pseudo code

--- evenOccurrences/evenOccurrences.js
+++ evenOccurrences/evenOccurrences.js
@@ -12,4 +12,18 @@
 
 const evenOccurence = (arr) => {
   // Your code here.
+  // My initial plan:
+  // 1) separate unique & non-unique elements into their own arrays
+  // 2) compare elements of one to elements of the other
+  // 3) if the elements match, assign a counter to each matching occurance
+  // 4) collect counts into array
+  // 5) return 1st instance of highest even number
+  const uniques = arr.sort();
+    // for (let i = 0; i < uniques.length; i++) {
+    //   return uniques[i] === uniques[i + 1] ? false : true
+    // }
+  return uniques;
 };
+
+console.log(evenOccurence([1,2,3,4]));
+console.log(evenOccurence([1,2,3,3,1,12,18,6,12]));
