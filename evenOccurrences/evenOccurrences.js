9f9417d81888bd99ca5af2cbdb3d1edc7cd02f85
Antonio Melendez <antoniomelendez@Antonios-MacBook-Pro.local>
2017-07-17 10:48:09 -0600

    evenOccurrence - Complte.

--- evenOccurrences/evenOccurrences.js
+++ evenOccurrences/evenOccurrences.js
@@ -9,7 +9,21 @@
  *   * const onlyEven = evenOccurence([1, 7, 2, 4, 5, 1, 6, 8, 9, 6, 4, 1]);
  *    * console.log(onlyEven); //  4
  *    */
-
-const evenOccurence = (arr) => {
-  // Your code here.
-};
+ const evenOccurence = (arr) => {
+   let numCheck = arr[0];
+   for (let i = 0; i < arr.length; i++) {
+     let count = 0;
+     numCheck = arr[i];
+     for (let j = 0; j < arr.length; j++) {
+       if (numCheck === arr[j]) {
+         count++;
+       }
+     }
+     if (count != 0 && count % 2 === 0){
+           return numCheck;
+     }
+   }
+   return null;
+ };
+ const onlyEven = evenOccurence([1, 7, 2, 4, 5, 1, 6, 8, 9, 6, 4, 1]);
+ console.log(onlyEven);
