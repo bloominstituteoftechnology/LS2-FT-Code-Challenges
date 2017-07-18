99331eb1c1ee4bbdc272750b79db82fecfc5aa7b
Gregory Knudsen <dev.artist15@gmail.com>
2017-07-18 12:35:58 -0400

    both ES5 & ES6 versions complete

--- forLoopTimeout/forLoopTimeout.js
+++ forLoopTimeout/forLoopTimeout.js
@@ -4,10 +4,24 @@
 // The output should be 1, 2, 3, .... 10. Right now it just prints 11.
 // I've been asked this three times in separate interviews.
 
-for (var i = 1; i <= 10; i++) {
-	setTimeout(function() {
+// ES6 - just change var to let
+for (let i = 1; i <= 10; i++) {
+	// (function() {
+		setTimeout(function() {
 		// From looking at the code you would assume it would print 1 - 10
 		// It doesn't.  Why?  How can you make it print 1 - 10.
 		console.log(i);
-	}, 0);
-}
+		}, 0);
+	};
+
+
+
+//ES5 - create an IIFE (Immediately Invoked Function Expression, which forces the loop to pass the value to i in each setTimeout call)
+// for(var i = 1; i <= 10; i++) {
+//   // console.log('Im in counter ' + i);
+//   (function(index) {
+//     setTimeout(function() {
+//         console.log(index);
+//     },0);
+//   })(i);
+// };
