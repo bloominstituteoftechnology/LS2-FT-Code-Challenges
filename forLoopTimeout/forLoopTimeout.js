0b36e92c6501635498347944e840da75fd008b70
Nana Owusu Nyamekye <nanaowusu1234@gmail.com>
2017-07-18 16:47:24 +0000

    Answer for 'forLoopTimeout' challenge

--- forLoopTimeout/forLoopTimeout.js
+++ forLoopTimeout/forLoopTimeout.js
@@ -11,3 +11,7 @@ for (var i = 1; i <= 10; i++) {
 		console.log(i);
 	}, 0);
 }
+
+// When setTimeout is used with a function as the first argument and its timer set to 0, 
+// the setTimeout block of code loses its chance to be executed immediately since JavaScript is single-threaded.
+// What I would do to make the code print from 1 - 10 will be to bring the console.log(i) statement out of the setTimeout method.
