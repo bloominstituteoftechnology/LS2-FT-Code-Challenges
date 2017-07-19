9864a8c95d826fc31ab8ff566df76460cca99b82
MuraraAllan <allan.murara@gmail.com>
2017-07-19 18:00:12 -0300

    variable unused beeing passed

--- binarySearch/binarySearch.js
+++ binarySearch/binarySearch.js
@@ -1,64 +1,34 @@
 /*
- * Given a sorted array, find the index of the specified element
- * using binary search.
- * https://www.khanacademy.org/computing/computer-science/algorithms/binary-search/a/binary-search
- * https://en.wikipedia.org/wiki/Binary_search_algorithm
- * */
+* Given a sorted array, find the index of the specified element
+* using binary search.
+* https://www.khanacademy.org/computing/computer-science/algorithms/binary-search/a/binary-search
+* https://en.wikipedia.org/wiki/Binary_search_algorithm
+* */
 
 /**
- *  * const index = binarySearch([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 2);
- *	* console.log(index); // 1
+*  * const index = binarySearch([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 2);
+*	* console.log(index); // 1
 **/
 // i've made two push to repo lack of knowledge. I've creat a temp handler to tag where the array is not supposed go to and put a random extra  guesser trying to find by random
 // can you guyes iterate over it with me and shows me? :)
 const binarySearch = (nums, target) => {
-	let max_index = nums.length -1; 
-	let min_index = 0;
-  let biggguess = Math.round((max_index+min_index)/2);
-  let lessguess = Math.round((max_index+min_index)/2);
-  let randguess = Math.round(Math.random((max_index+min_index)/2));
-  let randguess2 = Math.round(max_index);
-  let no_search = [];
-  let x = 0;
-
-  console.log(x)
-  console.log(randguess)
+  let max_index = nums.length -1; 
+  let min_index = 0;
+  let guess = Math.floor((max_index+min_index)/2);
   const test = () => {
-    // limiterc
-    if (min_index > nums.length) return false;
-    // randguess check
-    if (randguess === target) return randguess;
-    if (ranguess2 === target) return randguess2;
-    // is the lessguess or the less the right target?
-    if (nums[biggguess] === target)  return biggguess;
-    if (nums[lessguess] === target)  return lessguess;
-    // should go up-front if its already checked by the guesser
-    if (no_search.includes(min_index)) min_index++;
-    if (no_search.includes(max_index)) max_index--;
-    // should check first and last current unchecked position
-    if (nums[max_index] === target) return max_index; 
-    if (nums[min_index] === target) return min_index;
-    // adding values to first and last item
-    max_index--;
-    min_index++
-    x++;
-    // no_search controller
-    no_search.push(randguess);
-    no_search.push(biggguess);
-    no_search.push(lessguess);
-    // hand_gues reasign rand value
-    randguess = Math.random((max_index+min_index)/2);
-    biggguess--;
-    lessguess++;
-    return test(nums);
-  };
+      if (min_index > nums.length) return false;
+      if (nums[guess] === target)  return guess;
+      if (nums[max_index] === target) return max_index; 
+      if (nums[min_index] === target) return min_index;
+      max_index--;
+      min_index++
+      if (nums[guess] > target) guess = guess -1;
+      else guess = guess +1;
+      i++;
+      return test(nums);
+    };
   return test();
 };
-
-let arr = [];
-  for(i=1; i<5000; i++){
-    arr.push(i);
-  }
-
 const index = binarySearch(arr,3398);
-  console.log(index); // 1
+const index = binarySearch([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 10);
+console.log(index);
--- evenOccurrences/evenOccurrences.js
+++ evenOccurrences/evenOccurrences.js
@@ -8,53 +8,6 @@
  *   * const onlyEven = evenOccurence([1, 7, 2, 4, 5, 1, 6, 8, 9, 6, 4, 1]);
  *    * console.log(onlyEven); //  4
  *    */
-<<<<<<< 7b6681d230adfa141db77ace55a222988807f8a2
-// should iterate over array and check if it already exists with set
-const evenOccurence = (arr) => {
-  const set = new Set();
-// go around set and remove current position of the array 
-const tail = (arr, index) => {
-  let newarray = [];
-  const getelement = (arr, index, pos2 = arr.length) => {
-    if (index === pos2) return newarray 
-      newarray.push(arr[index])
-    index++;
-    return getelement(arr, index);
-  } 
-  return getelement(arr, index);
-}
-const iteration = (arr, index = 0) => {
-  if (index === arr.length ) return null;
-  if (new Set(tail(arr,index+1)).has(arr[index])) return arr[index];
-  index++;
-  return iteration(arr, index);
-}
-return iteration(arr);
-};
-const onlyEven = evenOccurence([1,7,1, 2,7,3, 4, 5, 6, 8, 9]);
-// 
-const tail = (arr, index) => {
-  let newarray = [];
-  const getelement = (arr, index, pos2 = arr.length) => {
-    if (index === pos2) return newarray;
-    newarray.push(arr[index])
-    index++;
-    return getelement(arr,index);
-  } 
-  return getelement(arr, index)
-}
-const iteration = (arr, index = 0) => {
-  if (index === arr.length ) {  return null; }
-  if (new Set(tail(arr,index+1)).has(arr[index])) return arr[index];
-  index++;
-  return iteration(arr, index)
-}
-return iteration(arr)
-};
-
-const onlyEven = evenOccurence([1, 7,1, 2,7,3, 4, 5, 6, 8, 9]);
-=======
-//should iterate over array and check if it already exists with set
 const evenOccurence = (arr) => {
 	const set = new Set();
   // go around set and remove current position of the array 
@@ -77,4 +30,4 @@ const evenOccurence = (arr) => {
   return iteration(arr);
 };
 const onlyEven = evenOccurence([1,7,1, 2,7,3, 4, 5, 6, 8, 9]);
->>>>>>> Indent
+  
