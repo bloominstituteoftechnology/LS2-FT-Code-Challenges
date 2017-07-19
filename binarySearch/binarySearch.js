cc46ebbc1a68f25c8d37666303c2ff40d8808fb8
MuraraAllan <allan.murara@gmail.com>
2017-07-19 17:34:54 -0300

    binarySearch added

--- binarySearch/binarySearch.js
+++ binarySearch/binarySearch.js
@@ -11,5 +11,28 @@
 **/
 
 const binarySearch = (nums, target) => {
+	let max_index = nums.length -1; 
+	let min_index = 0;
+  let i = 0;
+  let guess = Math.round((max_index+min_index)/2);
 
+  const test = () => {
+    //check for equal control
+    if (min_index > nums.length) return false;
+    if (nums[guess] === target)  return guess;
+    if (nums[max_index] === target) return max_index; 
+    if (nums[min_index] === target) return min_index;
+    max_index--;
+    min_index++
+    //guess controller
+    if (nums[guess] > target) guess = guess -1;
+    else guess = guess +1;
+    i++;  
+    return test(nums);
+  };
+  return test();
 };
+
+
+const index = binarySearch([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 10);
+  console.log(index); // 1
