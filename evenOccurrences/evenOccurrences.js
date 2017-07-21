6c42a049e9c93980438c5b5864afea7b006e3972
MuraraAllan <allan.murara@gmail.com>
2017-07-21 15:13:08 -0300

    evenOcurences

--- evenOccurrences/evenOccurrences.js
+++ evenOccurrences/evenOccurrences.js
@@ -8,6 +8,7 @@
  *   * const onlyEven = evenOccurence([1, 7, 2, 4, 5, 1, 6, 8, 9, 6, 4, 1]);
  *    * console.log(onlyEven); //  4
  *    */
+<<<<<<< 7b6681d230adfa141db77ace55a222988807f8a2
 // should iterate over array and check if it already exists with set
 const evenOccurence = (arr) => {
   const set = new Set();
@@ -35,8 +36,8 @@ const onlyEven = evenOccurence([1,7,1, 2,7,3, 4, 5, 6, 8, 9]);
 const tail = (arr, index) => {
   let newarray = [];
   const getelement = (arr, index, pos2 = arr.length) => {
-    if (index === pos2) { return newarray }
-      newarray.push(arr[index])
+    if (index === pos2) return newarray;
+    newarray.push(arr[index])
     index++;
     return getelement(arr,index);
   } 
@@ -52,3 +53,28 @@ return iteration(arr)
 };
 
 const onlyEven = evenOccurence([1, 7,1, 2,7,3, 4, 5, 6, 8, 9]);
+=======
+//should iterate over array and check if it already exists with set
+const evenOccurence = (arr) => {
+	const set = new Set();
+  // go around set and remove current position of the array 
+  const tail = (arr, index) => {
+    let newarray = [];
+    const getelement = (arr, index, pos2 = arr.length) => {
+      if (index === pos2) return newarray 
+      newarray.push(arr[index])
+      index++;
+      return getelement(arr, index);
+    } 
+    return getelement(arr, index);
+  }
+	const iteration = (arr, index = 0) => {
+    if (index === arr.length ) return null;
+    if (new Set(tail(arr,index+1)).has(arr[index])) return arr[index];
+    index++;
+    return iteration(arr, index);
+	}
+  return iteration(arr);
+};
+const onlyEven = evenOccurence([1,7,1, 2,7,3, 4, 5, 6, 8, 9]);
+>>>>>>> Indent
