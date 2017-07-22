7abf19cf18255a7af2d77f8a210022ad22a7e7e0
Chooster <jcooleycoder@gmail.com>
2017-07-22 09:37:43 -0700

    Finished String Compression

--- stringCompression/stringCompression.js
+++ stringCompression/stringCompression.js
@@ -4,3 +4,20 @@
 // If the "compressed" string would not become smaller than the original string,
 // your method should return the original string.
 // You can assume the string has only uppercase and lowercase letters (a - z).
+
+const strComp = (str) => {
+  let strTemp = '';
+  let temp = str[0];
+  let count = 0;
+  for (let i = 0; i <= str.length; i++) {
+    if (str[i] !== temp) {
+      strTemp += temp;
+      strTemp += count;
+      count = 1;
+      temp = str[i];
+    }
+    else count++;
+  }
+  if (strTemp.length >= str.length) return str;
+  return strTemp;
+};
