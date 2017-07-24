7076dcbb0ee9c4644417ea7f8633f8b675592a02
Gregory Knudsen <dev.artist15@gmail.com>
2017-07-24 15:22:56 -0400

    divide works

--- operators/operators.js
+++ operators/operators.js
@@ -8,11 +8,36 @@
 const multiply = (a, b) => {
     let result = 0;
     for (let i = 0; i < b; i++) {
-        // console.log(result)
         result += a;
     }
     return result;
 }
 
-console.log(multiply(3,5));
-console.log(multiply(5,30));
+console.log(multiply(3,5) === 15); // true
+console.log(multiply(5,30) === 150); // true
+
+
+
+const divide = (a, b) => {
+    let count = 1;
+
+    while (a > b) {
+        a -= b;
+        count++;
+    }
+    return count;
+}
+
+
+
+console.log(divide(10, 2) === 5); // true
+console.log(divide(12, 6) === 2); // true
+
+
+// const modulo = (a, b) => {
+//     return 2;
+// }
+//
+//
+//
+// console.log(modulo(10, 4) === 5); // true
