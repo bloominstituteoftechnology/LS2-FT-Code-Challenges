ccdf7d9f835960497414e031cbfe979d18fc7b6c
sunjieming <ben@lambdaschool.com>
2017-07-25 08:57:46 -0700

    Added breadthFirstSearch

---
+++ breadthFirstSearch/breadthFirstSearch.js
@@ -0,0 +1,16 @@
+/*
+ * Write a function that accepts a tree data structure and a value to search for.
+ * Search for the value using a breadth-first search algorithm.
+ * Example:
+ * const tree = {
+ *	x: 1,
+ *	y: 1,
+ *	z: {
+ *		x: 1,
+ *		y: 1,
+ *		z: 1,
+ *		},
+ *	a: 2,
+ * };
+ * breadthFirstSearch(tree, 2);// will return true before it recursively searches `z`
+ */
